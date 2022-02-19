a2667fadf3c5db18e7cfa94f979a10ab82a96142
VANSHIKA8421 <58525932+VANSHIKA8421@users.noreply.github.com>
2022-02-19 15:35:34 +0530

    Time: 30 ms (30.72%), Space: 58.1 MB (5.02%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,57 @@
+class Solution 
+{
+    public int numberOfBeams(String[] bank) 
+    {
+        int n = 0;
+        n= bank[0].length();
+        
+        
+        System.out.println(n);
+        
+        
+        int count =0;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for( String i : bank)
+        {
+            count = 0;
+            for(char j :i.toCharArray())
+            {
+                if(j=='1')
+                {
+                    count++;
+                }
+            }
+            if(count>0)
+            list.add(count);
+        }
+        int ans= 0;
+        for(int i = 0 ; i < list.size()-1 ; i++)
+        {
+            ans = ans + list.get(i)*list.get(i+1);
+        }
+        return ans;
+    }
+}
+// public int numberOfBeams(String[] bank) {
+//         List<Integer> freq=new ArrayList<>();
+//         int index=0;
+//         for(String s: bank){
+//             int count=0;
+//             for(char ch: s.toCharArray()){
+//                 if(ch=='1'){
+//                     count++;
+//                 }
+//             }
+//             if(count>0){
+//                 freq.add(count);
+//             }
+//         }
+//         int res=0;
+//         for(int i=0;i<freq.size()-1;i++){
+//             int temp=freq.get(i)*freq.get(i+1);
+//             res+=temp;
+//         }
+//         return res;
+        
+//     }
