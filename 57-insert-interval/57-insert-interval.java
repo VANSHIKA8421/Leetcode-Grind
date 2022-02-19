d8dd7c21e0602f7b6aef3945e559e7ac10bf0211
VANSHIKA8421 <58525932+VANSHIKA8421@users.noreply.github.com>
2022-02-19 14:09:01 +0530

    Time: 2 ms (73.12%), Space: 47.1 MB (28.78%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,53 +1,29 @@
 class Solution 
 {
-    // we can classify all the kind of intervals in 3 types
-    // all the intervals occuring before the new interval
-    // intervlas whose stand and end points can be merged eith new interval
-    // all the intervals occuring after end 
-    
     public int[][] insert(int[][] intervals, int[] newInterval) 
     {
         ArrayList<int[]> list = new ArrayList<>();
+        int n = intervals.length;
         int i = 0;
         
-        while(i<intervals.length&&intervals[i][1]<newInterval[0])
+        while(i<n  &&intervals[i][1] < newInterval[0])
         {
             list.add(intervals[i]);
             i++;
         }
-        while(i<intervals.length&&intervals[i][0]<=newInterval[1])
+        
+         while(i<intervals.length&&intervals[i][0]<=newInterval[1])
         {
             newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
             newInterval[1]=Math.max(newInterval[1],intervals[i][1]);
             i++;
         }
         list.add(newInterval);
-        while(i<intervals.length)
+        while(i<n)
         {
             list.add(intervals[i]);
             i++;
         }
-        return list.toArray(new int[list.size()][]);
+        return list.toArray(new int[list.size()][2]);
     }
 }
-// List<int[]> result = new ArrayList<>();
-// int index = 0;
-// // Add all non-overlapping intervals before the 'newSpan'
-// while (index < intervals.length && intervals[index][1] < newSpan[0]) {
-// 	result.add(intervals[index++]);
-// }
-// // Merge all overlapping intervals into the newSpan
-// while (index < intervals.length && intervals[index][0] <= newSpan[1]) {
-// 	int[] thisSpan = intervals[index++];
-// 	newSpan[0] = Math.min(thisSpan[0], newSpan[0]);
-// 	newSpan[1] = Math.max(thisSpan[1], newSpan[1]);
-
-// }
-// result.add(newSpan);  // return the merged newspan
-
-// // Add all remaining non-overlapping intervals after the merged newSpan
-// while (index < intervals.length) {
-// 	result.add(intervals[index++]);
-// }
-
-// return result.toArray(new int[result.size()][]);
