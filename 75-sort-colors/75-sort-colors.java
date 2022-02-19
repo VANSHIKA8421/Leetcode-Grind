ed439fd452cb024e74282d4510b8a9ca219810b5
VANSHIKA8421 <58525932+VANSHIKA8421@users.noreply.github.com>
2022-02-19 09:27:19 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (36.95%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -5,70 +5,28 @@ class Solution
         int left = 0;
         int right = nums.length-1;
         int mid = 0;
-        while(mid<=right)
+        
+        while(mid<=right){
+        if(nums[mid]==0)
         {
-            if(nums[mid]==0)
-            {
-                swap(mid,left,nums);
-                mid++;
-                left++;
-            }
-            else if( nums[mid]==1)
-            {
-                mid++;
-            }
-            else
-            {
-                swap(mid,right,nums);
-                right--;
-            }
+            swap(mid,left,nums);
+            mid++;
+            left++;
         }
+        else if(nums[mid]==1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(mid,right,nums);
+            right--;
+        }}
     }
-    public void swap(int left, int right, int[] nums)
+    void swap(int mid, int left, int[] nums)
     {
-        int temp = 0;
-        temp = nums[right];
-        nums[right] = nums[left];
-        nums[left] = temp;
+        int temp = nums[left];
+        nums[left]=nums[mid];
+        nums[mid]=temp;
     }
 }
-
-// public void swap(int[] nums, int i, int j) 
-// {
-// 	int temp = nums[i];
-// 	nums[i] = nums[j];
-// 	nums[j] = temp;
-// }
-
-// Arrays.sort();
-
-// counting sort
-
-// [ 0, 0, 1, 1, 2, 2 ]
-
-
-// [ 2, 0, 2, 1, 1, 0 ]
-
-
-//  0's |            | 2's 
-//   | travelling 
-
-
-// nums[travelling]==0 || nums[travelling]==2
-// nums[i] is either 0, 1, or 2.
-
-/// [ 0, 0, 1, 1, 2, 2 ]
-//             T
-//          0  2
-
-
-// if nums[travelling]==0
-// swaps(nums[travelling],nums[0])
-// 0++ travelling++
-
-// if nums[travelling]==2
-// swaps(nums[travelling],nums[0])
-// 2--
-
-// if nums[travelling]==1
-// travelling++
