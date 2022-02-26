3fa195b640ab08e24a7cf56e3624cf631397acaa
VANSHIKA8421 <58525932+VANSHIKA8421@users.noreply.github.com>
2022-02-26 10:55:08 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (26.81%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -2,22 +2,41 @@ class Solution
 {
     public int climbStairs(int n) 
     {
-        int[] dp = new int[n+1];
         
-        dp[0]=1;
-        dp[1]=1;
+        // MOST OPTIMISED SOLUTION
+        
+        
+       int prev =1;
+       int prev2 =1;
+        int current = 0; 
         
         for(int i = 2 ; i <= n ; i++)
         {
-            if(dp[i]==0)
-            {
-                dp[i]=dp[i-1]+dp[i-2];
-            }
+           current = prev + prev2;
+            prev2 = prev;
+            prev = current;
+            
         }
-        return dp[n];
+        return prev;
     }
 }
 // RECURSION TLE 
 //          if(n==0) return 1;
 //          if(n==1) return 1;
 //          else return climbStairs(n-1)+climbStairs(n-2);
+
+// CONVERTED RECURSION TO MEMOIZATION 
+//         int[] dp = new int[n+1];
+        
+//         dp[0]=1;
+//         dp[1]=1;
+        
+//         for(int i = 2 ; i <= n ; i++)
+//         {
+//             if(dp[i]==0)
+//             {
+//                 dp[i]=dp[i-1]+dp[i-2];
+//             }
+//         }
+//         return dp[n];
+
