699dfea31a10a2b6d57dedbb8b2e2528c66c5302
VANSHIKA8421 <58525932+VANSHIKA8421@users.noreply.github.com>
2022-03-07 01:06:03 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (56.14%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -9,15 +9,21 @@
  *     }
  * }
  */
-public class Solution { //java version
-    public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
+public class Solution 
+{
+    public boolean hasCycle(ListNode head) 
+    {   
         ListNode slow = head;
-        while(fast != null && fast.next != null){            
-            fast = fast.next.next; //2 times
-            slow = slow.next;
-            if(fast == slow) return true;
+        ListNode fast = head;
+        
+        
+        while(fast != null && fast.next != null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+            if(slow==fast) return true;
         }
         return false;
+        
     }
 }
