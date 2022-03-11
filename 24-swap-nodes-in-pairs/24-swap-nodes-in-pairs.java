76e6bce73472b16623ddd029dd7b7c1e17ca62bd
VANSHIKA8421 <58525932+VANSHIKA8421@users.noreply.github.com>
2022-03-11 13:26:55 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (19.86%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -8,23 +8,23 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
-
-public ListNode swapPairs(ListNode head) {
-    
-    if(head == null) return head;
-    ListNode current = head;
-    while(current.next != null){
-        int t = current.val;
-        current.val = current.next.val;
-        current.next.val = t;
-        
-        if(current.next.next != null){
-            current = current.next.next;
-        } else {
-            break;
+class Solution 
+{
+    public ListNode swapPairs(ListNode head) 
+    {
+        ListNode dummy = new ListNode(0);
+        dummy.next=head;
+        ListNode point = dummy;
+        while(point.next!=null && point.next.next!=null)
+        {
+            ListNode swap1 = point.next;
+            ListNode swap2 = point.next.next;
+            point.next = swap2;
+            
+            swap1.next=swap2.next;
+            swap2.next=swap1;
+            point=swap1;
         }
-    }
-    return head;
+        return dummy.next;
     }
 }
