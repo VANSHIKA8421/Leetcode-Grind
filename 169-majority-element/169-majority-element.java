83a32f790d72f8a9eae69c2556906cc3655a6009
VANSHIKA8421 <58525932+VANSHIKA8421@users.noreply.github.com>
2022-03-15 16:41:53 +0530

    Time: 1 ms (99.96%), Space: 56.5 MB (30.49%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -2,14 +2,14 @@ class Solution
 {
     public int majorityElement(int[] nums) 
     {
-        int elem = 0; int count = 0;
-       for(int i = 0 ; i < nums.length ; i++)
-       {
-           if (count==0)
-             {
-                 elem=nums[i];
-             }
-            if( nums[i] == elem)
+        int maj = 0; int count = 0;
+        for(int i = 0; i < nums.length ; i++)
+        {
+            if(count==0)
+            {
+                maj=nums[i];
+            }
+            if(nums[i]==maj)
             {
                 count++;
             }
@@ -17,8 +17,9 @@ class Solution
             {
                 count--;
             }
-       }
-        return elem;
+            
+        }
+        return maj;
     }
 }
 // MOORE VOTING ALGORITHM
